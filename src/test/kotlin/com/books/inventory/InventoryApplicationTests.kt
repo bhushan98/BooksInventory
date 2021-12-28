4505359f360291f660e0f037385e0de309d187e5
Bhushan Sonawane <bhushan.sonawane@Bhushans-MacBook-Pro.local>
2021-12-28 11:06:46 +0530

    First Commit

---
+++ src/test/kotlin/com/books/inventory/InventoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.books.inventory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
